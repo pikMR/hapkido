738c7c25223c8ceece0fc55b29e01959f8499376
Benjamin Garcia Hernandez <benjahergar@gmail.com>
2015-04-13 13:08:26 +0200

    modificacion minusculas-mayusculas

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -1,6 +1,5 @@
 <?php
 namespace App\Http\Middleware;
-use App\Http\Middleware\isType;
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
