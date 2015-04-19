15e698a6eea9f4fd32a405bc049df59770dff1c2
Benjamin Garcia Hernandez <benjahergar@gmail.com>
2015-04-20 01:35:43 +0200

    Ultimos retoques pasafotos y cabecera 2

--- resources/views/pasafotos.blade.php
+++ resources/views/pasafotos.blade.php
@@ -2,13 +2,13 @@
 <div class="windy-demo">
 					<ul id="wi-el" class="wi-container">
 						<!--li><img src="images/resized/s_1.jpg" alt="image1"/><h4>Práctica</h4><p>El camino es largo, y la práctica es fundamental.</p></li-->
-						<li><img src="images/resized/s_2.jpg" alt="image2"/><h4>Reconocimiento</h4><p>Todo trabajo bien echo debe de ser reconocido.</p></li>
-						<li><img src="images/resized/s_3.jpg" alt="image3"/><h4>Respeto</h4><p>El respeto a tu mentor en cualquien momento y situación.</p></li>
-						<li><img src="images/resized/s_4.jpg" alt="image4"/><h4>Humildad</h4><p>Hay que mirar a los ojos por igual, todos tenemos un comienzo. </p></li>
-						<li><img src="images/resized/s_5.jpg" alt="image5"/><h4>Paciencia</h4><p>Todo lleva su tiempo, y cada cosa tiene su momento.</p></li>
-						<li><img src="images/resized/s_6.jpg" alt="image6"/><h4>El maestro</h4><p>Tu consejero en el camino.</p></li>
-						<li><img src="images/resized/s_7.jpg" alt="image7"/><h4>Las costumbres</h4><p>Siempre respeto por el pasado.</p></li>
-						<li><img src="images/resized/s_8.jpg" alt="image8"/><h4>Tu comienzo</h4><p>Aquí empiezas tú.</p></li>
+						<li><img src="images/resized/s_2.JPG" alt="image1"/><h4>Reconocimiento</h4><p>Todo trabajo bien echo debe de ser reconocido.</p></li>
+						<li><img src="images/resized/s_3.JPG" alt="image2"/><h4>Respeto</h4><p>El respeto a tu mentor en cualquien momento y situación.</p></li>
+						<li><img src="images/resized/s_4.JPG" alt="image3"/><h4>Humildad</h4><p>Hay que mirar a los ojos por igual, todos tenemos un comienzo. </p></li>
+						<li><img src="images/resized/s_5.JPG" alt="image4"/><h4>Paciencia</h4><p>Todo lleva su tiempo, y cada cosa tiene su momento.</p></li>
+						<li><img src="images/resized/s_6.JPG" alt="image5"/><h4>El maestro</h4><p>Tu consejero en el camino.</p></li>
+						<li><img src="images/resized/s_7.JPG" alt="image6"/><h4>Las costumbres</h4><p>Siempre respeto por el pasado.</p></li>
+						<li><img src="images/resized/s_8.JPG" alt="image7"/><h4>Tu comienzo</h4><p>Aquí empiezas tú.</p></li>
 						<!--li><img src="images/demo1/14.jpg" alt="image14"/><h4>Blessed Fever</h4><p>Twee biodiesel fugiat ut. Thundercats mcsweeney's occaecat brooklyn mollit.</p></li>
 						<li><img src="images/demo1/15.jpg" alt="image15"/><h4>Origins</h4><p>Quinoa marfa banksy nostrud brunch bushwick PBR. Mcsweeney's bushwick banksy.</p></li>
 						<li><img src="images/demo1/16.jpg" alt="image16"/><h4>Fire Lime</h4><p>Brunch fugiat locavore, swag photo booth keffiyeh labore small batch.</p></li>
