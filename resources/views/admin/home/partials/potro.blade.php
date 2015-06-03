595d59dd58efe1fa38adf7e66bc6caddb6811e03
Benjamin Garcia Hernandez <benjahergar@gmail.com>
2015-06-03 11:32:32 +0200

    actualizacion de dropbox

--- resources/views/admin/home/partials/potro.blade.php
+++ resources/views/admin/home/partials/potro.blade.php
@@ -1,5 +1,5 @@
 <div class="col-xs-6 col-md-4">
-    <a href="https://www.dropbox.com/sh/jmm8btc9ntygndp/AAC2W2GbSGQxgGZCcx33zYwJa?dl=0" class="thumbnail" target='_blank'>
+    <a href="https://www.dropbox.com/sh/n8wqrpe692gwruo/AAAmtKcT4af_V66QSnRZIxz0a?dl=0" class="thumbnail" target='_blank'>
         <img src="images/admin/dropbox2.png" alt="Acceso a dropbox" style="width: 200px;height: 200px;">
     </a> 
     <p class="text-center">Dropbox</p>
