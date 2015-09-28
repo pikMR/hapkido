ae51faa6b4670b2e3358f4c551870a35b8432a15
Benjamin Garcia Hernandez <benjahergar@gmail.com>
2015-09-28 17:17:31 +0200

    reajuste de la ventana de twitter y youtube

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -123,11 +123,11 @@
         </div>
       
         <div class="container" style="margin-top: 0px;"> 
-           <div class="five columns" style="right: 0px;top:280px;">
-       <a class="twitter-timeline"  href="https://twitter.com/HAPKIDO_MURCIA" data-widget-id="574874501479075841">Tweets por el @HAPKIDO_MURCIA.</a>
+           <div class="twelve columns" style="right: 0px;top:330px;">
+       <a class="twitter-timeline"  href="https://twitter.com/HAPKIDO_MURCIA" data-widget-id="574874501479075841">Tweets de @HAPKIDO_MURCIA.</a>
         <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 
-    </div> <!-- ahora incluimos youtube -->
+            </div> <!-- ahora incluimos youtube -->
             @include ('multimedia') 
             <div class="col-sm-pull-0">
                 <div class="persoimagen columns" id="media">
