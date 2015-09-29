b7b66dbe13978f285bdec7bc10ff50f16f4700e8
Benjamin Garcia Hernandez <benjahergar@gmail.com>
2015-09-29 19:06:03 +0200

    avanzado

--- resources/views/admin/users/avanzado.blade.php
+++ resources/views/admin/users/avanzado.blade.php
@@ -1,12 +1,28 @@
 @extends('app')
 @section('content')
-<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
- <link href="css/app.css" rel="stylesheet"  type="text/css">
 <div class="container">
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
-				<div class="panel-heading">Sigue el camino</div>
+                            
+				<div class="panel-heading">
+                                    <h4 style="text-align: center">Sigue el camino</h4>
+                                 <object data="../data/2909201500.pdf" type="application/pdf" width="100%" height="900px">
+ 
+  <p> Tienes problemas para visualizar el archivo, pincha en el siguiente enlace para descargar :  <a href="2909201500.pdf">DESCARGA!
+</a></p>
+  
+</object>
+                                    
+                                    <object data="../data/2909201501.pdf" type="application/pdf" width="100%" height="900px">
+ 
+  <p> Tienes problemas para visualizar el archivo, pincha en el siguiente enlace para descargar :  <a href="2909201500.pdf">DESCARGA!
+</a></p>
+  
+</object>
+                                    <iframe src="https://docs.google.com/presentation/d/1BeXeW1j-A6v13hiRp1OcEmuYNiyZ0jD9bjv0hviKPpk/embed?start=true&loop=false&delayms=3000" frameborder="0" width="960" height="749" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+                                </div>
+                                
                                 <br>
 			</div>   
 		</div>
