af151008d86f4d9a4988eee3f5bb520d5dafa0f1
Benjamin Garcia Hernandez <benjahergar@gmail.com>
2015-10-06 17:47:38 +0200

    cambios en privilegios en tipos de usuario

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -9,6 +9,44 @@
                             
 				<div class="panel-heading">Bienvenido {{ $usuario->getFullNameAttribute()}}</div>
                                 <br>
+                                  
+                                @if(!$usuario->isAdmin())
+                                        @if($usuario->is('user'))
+                                        <div class="col-lg-push-6">
+                                             <h1 class="text-center">Preguntanos o Sugiere algo</h1>
+                                            <a href="help" class="thumbnail">
+                                                <img src="images/admin/question.png" alt="Preguntanos algo" >
+                                            </a> 
+                                           
+                                        </div>
+                                        @else
+                                        <h1 class="text-center">Dropbox</h1>
+                                        <div class="col-xs-6 col-lg-12">
+                                            <a href="https://www.dropbox.com/sh/n8wqrpe692gwruo/AAAmtKcT4af_V66QSnRZIxz0a?dl=0" class="thumbnail" target='_blank'>
+                                                <img src="images/admin/dropbox2.png" alt="Acceso a dropbox" style="width: 200px;height: 200px;">
+                                            </a> 
+                                            
+                                        </div>
+                                        <h1 class="text-center">Comprueba peticiones o dudas</h1>
+                                        <div class="col-xs-6 col-lg-12">
+                                            <a href="appeals" class="thumbnail">
+                                                <img src="images/admin/requests.png" alt="Comprueba preguntas" >
+                                            </a> 
+                                            
+                                        </div>
+                                        @endif
+                                @endif
+                                 
+                                @if ($usuario->isAdmin() || $usuario->is('contributor'))
+                                    <h1 class="text-center">Contenido Avanzado</h1>
+                                   <div class="col-xs-6 col-lg-12">
+                                        <a href="admin/avanzado" class="thumbnail">
+                                            <img src="images/admin/cinturon.png" alt="Acceso a contenido avanzado" >
+                                        </a> 
+                                        
+                                    </div>
+
+                                @endif
                                 @if ($usuario->isAdmin())
                                    <div class="row">
                                    @include('admin.home.partials.padmin')
@@ -16,36 +54,9 @@
                                 @else
                                     <div class="row col-md-offset-3">
                                        @include('admin.home.partials.potro')
-                                @endif    
-                                    @if  ( !($usuario->is('editor') || $usuario->is('contributor') || $usuario->is('subscriber')) ) 
-                                        @if(!$usuario->isAdmin())
-                                        <div class="col-md-8">
-                                            <a href="help" class="thumbnail">
-                                                <img src="images/admin/question.png" alt="Preguntanos algo" >
-                                            </a> 
-                                            <p class="text-center">Preguntanos o Sugiere algo</p>
-                                        </div>
-                                        @endif
-                                    @else
-                                        <div class="col-md-8">
-                                            <a href="appeals" class="thumbnail">
-                                                <img src="images/admin/requests.png" alt="Comprueba preguntas" >
-                                            </a> 
-                                            <p class="text-center">Comprueba peticiones o dudas</p>
-                                        </div>
-                                    @endif
-                                 </div>
-                                @if ($usuario->isAdmin() || $usuario->is('contributor'))
-                                 <div class="row col-md-offset-3">
-                                   <div class="col-lg-8 col-md-10">
-                                        <a href="admin/avanzado" class="thumbnail">
-                                            <img src="images/admin/cinturon.png" alt="Acceso a contenido avanzado" >
-                                        </a> 
-                                        <p class="text-center">Contenido Avanzado</p>
-                                    </div>
+                                @endif  
+                                    </div>   
                                 </div>
-                                @endif
-			</div>   
 		</div>
 	</div>
 </div>
--- resources/views/admin/home/partials/potro.blade.php
+++ resources/views/admin/home/partials/potro.blade.php
@@ -1,12 +1,30 @@
-<div class="col-xs-6 col-md-4">
+<!--div class="col-xs-6 col-md-4">
     <a href="https://www.dropbox.com/sh/n8wqrpe692gwruo/AAAmtKcT4af_V66QSnRZIxz0a?dl=0" class="thumbnail" target='_blank'>
         <img src="images/admin/dropbox2.png" alt="Acceso a dropbox" style="width: 200px;height: 200px;">
     </a> 
     <p class="text-center">Dropbox</p>
-</div>
-<div class="col-xs-6 col-md-4">
+</div-->
+<div class="col-xs-6 col-lg-push-1">
+    <h1 class="text-center">Mi perfil</h1>
     <a href="profile" class="thumbnail">
         <img src="images/admin/miperfil.png" alt="Acceso a mi perfil" >
     </a> 
-    <p class="text-center">Mi perfil</p>
+    
+</div>
+<div class="col-xs-12 col-sm-pull-2 ">
+    <h1 class="text-center">ESQUEMA ANTIGUO DE LA ESCUELA</h1>
+<object data="{{asset('data/viejo_esquema.pdf')}}" type="application/pdf" width="100%" height="900px">
+ 
+  <p> Tienes problemas para visualizar el archivo, pincha en el siguiente enlace para descargar :  <a href="{{asset('data/viejo_esquema.pdf')}}">DESCARGA!
+      </a></p></object>
+    <h1 class="text-center">NOMENCLATURAS</h1>
+<object data="{{asset('data/nomenclaturas.pdf')}}" type="application/pdf" width="100%" height="900px">
+ 
+  <p> Tienes problemas para visualizar el archivo, pincha en el siguiente enlace para descargar :  <a href="{{asset('data/nomenclaturas.pdf')}}">DESCARGA!
+</a></p></object>
+    <h1 class="text-center">VOCABULARIO</h1>
+<object data="{{asset('data/vocabulario.pdf')}}" type="application/pdf" width="100%" height="900px">
+ 
+  <p> Tienes problemas para visualizar el archivo, pincha en el siguiente enlace para descargar :  <a href="{{asset('data/vocabulario.pdf')}}">DESCARGA!
+</a></p></object>
 </div>
