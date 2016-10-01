e9c9768250d60a9fb6c99247b9fd4c847be0c175
pikmr <benjahergar@gmail.com>
2016-10-01 19:50:16 +0200

    pruebas -

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,6 +24,7 @@ class HomeController extends Controller {
 	 */
 	public function __construct(Guard $auth)
 	{
+				dd(1);
                 $this->auth = $auth;
 		$this->middleware('auth');
                 //$this->request = $request;
